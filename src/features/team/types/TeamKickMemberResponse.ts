9a76024af5e214e1f571478dae68fc4989ec988d
박승수 <qkrtmdtn0708@gmail.com>
2025-11-26 16:32:48 +0900

    fix:오류수정

--- src/features/team/types/TeamKickMemberResponse.ts
+++ src/features/team/types/TeamKickMemberResponse.ts
@@ -1,10 +1,7 @@
 // src/features/team/types/TeamKickMemberResponse.ts
-// 팀원 내보내기 API 응답 DTO
-// TODO: 해당 API의 응답 JSON 구조를 여기에 추가하세요.
-// 일반적으로 성공 여부만 간단히 알려줍니다.
 
 export interface TeamKickMemberResponse {
   code: number;
   msg: string;
-  data: {};
+  data: Record<string, never>;
 }
