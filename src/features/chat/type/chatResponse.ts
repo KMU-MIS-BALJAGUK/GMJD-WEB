0d79a7df5f6ed1c57aae56df3ce2385894ef607d
xuuwon <dumpling1011@gmail.com>
2025-11-30 00:20:17 +0900

    feat: 채팅 목록 api 연동

---
+++ src/features/chat/type/chatResponse.ts
@@ -0,0 +1,22 @@
+export type ChatRoomDTO = {
+  chatroomId: number;
+  contestInfo: {
+    contestId: number;
+    teamId: number;
+    contestName: string;
+    imageUrl: string;
+  };
+  lastChatInfo: {
+    lastMessage: string;
+    lastMessageAt: string;
+    unReadMessageCount: number;
+  };
+};
+
+export type ChatRoomListResponse = {
+  code: number;
+  msg: string;
+  data: {
+    chatRooms: ChatRoomDTO[];
+  };
+};
